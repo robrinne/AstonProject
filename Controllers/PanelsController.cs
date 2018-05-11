65585bb95d1f2daeaada562d58ec6df29ebfa1f4
Robin <Robin@robin.fr>
2018-05-12 01:59:20 +0200

    Premier commit

---
+++ Controllers/PanelsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AstonProject.Controllers
+{
+    public class PanelsController : Controller
+    {
+        // GET: Panels
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/TableController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AstonProject.Controllers
+{
+    public class TableController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

446359ea734cf56e508f7f8b3a828210c754eddc
Robin <Robin@robin.fr>
2018-05-14 20:19:44 +0200

    Création des controlleurs et vues pour la création de questionnaires et la pages des questionnaires

---
+++ Controllers/HistoryQuestionnaireController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AstonProject.Controllers
+{
+    public class HistoryQuestionnaireController : Controller
+    {
+        // GET: HistoryQuestionnaire
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
